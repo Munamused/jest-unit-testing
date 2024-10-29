af6d02afe978704b47f64b148a2fbce22b9a8917
Madison Lopez <madison.lopez.8404@gmail.com>
2024-10-29 10:23:51 -0700

    setting up done

---
+++ module.test.js
@@ -0,0 +1,12 @@
+import mut from './module.js'; // MUT = Module Under Test
+
+test('Testing sum -- success', () => {
+  const expected = 30;
+  const got = mut.sum(12, 18);
+  expect(got).toBe(expected);
+});
+
+test('Testing div -- success', () => {
+  const expected = 5;
+  const got = mut.div()
+});

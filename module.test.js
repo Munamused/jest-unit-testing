b65a2673d4c02d8862b16817f9705541d086a7cf
Madison Lopez <madison.lopez.8404@gmail.com>
2024-10-29 17:14:27 -0700

    Testing for div complete

--- module.test.js
+++ module.test.js
@@ -6,7 +6,45 @@ test('Testing sum -- success', () => {
   expect(got).toBe(expected);
 });
 
-test('Testing div -- success', () => {
+//|------------------------------ Testing for div ------------------------------|
+
+test('Testing div basic -- success', () => {
   const expected = 5;
-  const got = mut.div()
+  const got = mut.div(15, 3);
+  expect(got).toBe(expected);
 });
+
+test('Testing div first < second -- success', () => {
+  const expected = 0.5;
+  const got = mut.div(4, 8);
+  expect(got).toBe(expected);
+});
+
+test('Testing div by 0 -- success', () => {
+  const got = mut.div(10, 0);
+  expect(got).toBe(Infinity);
+});
+
+test('Testing div one negative -- success', () => {
+  const expected = -3;
+  const got = mut.div(33, -11);
+  expect(got).toBe(expected);
+});
+
+test('Testing div both negatives -- success', () => {
+  const expected = 5;
+  const got = mut.div(-20, -4);
+  expect(got).toBe(expected);
+});
+
+test('Testing div invalid input -- success', () => {
+  const got = mut.div('hey', -4);
+  expect(got).toBe(NaN);
+});
+
+test('Testing div infinity -- success', () => {
+  const got = mut.div(Infinity, 2);
+  expect(got).toBe(Infinity);
+});
+
+//|------------------------------ Testing for containsNumbers ------------------------------|
